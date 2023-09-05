813fc4e71d82d212514622952195f51cbe6bdf0f
UrielSJP12 <143725665+UrielSJP12@users.noreply.github.com>
2023-09-05 08:41:21 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+
+public class ListaTareas {
+
+   
+    public static void main(String[] args) 
+    {
+        
+        String tarea1 = "serie de ejercicios";
+        String  tarea2 = "sumas de octales";
+        String  tarea3 = "exposicion etica";
+        String  tarea4 = "tarea de investigacion";
+        String  tarea5 = "algoritmos";
+        
+       
+        System.out.println("la tarea 1  es: " +  tarea1);
+        System.out.println("la tarea  2 es: " +  tarea2);
+        System.out.println("la tarea 3 es: " +  tarea3);
+        System.out.println("la tarea  4 es: " +  tarea4);
+        System.out.println("la tarea  5 es: " +  tarea5);
+    }
+    
+}
